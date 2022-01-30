58fb8dd52f7f1dee67e063fdbdfe9ad9e3383aa5
Desmond Obisi <desmondcutest@gmail.com>
2022-01-30 19:35:32 +0100

    UI beta release

---
+++ src/configs/site-config.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  FaGithub,
+  FaDev,
+  FaLinkedin,
+  FaQuora,
+  FaTwitter
+} from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+
+const siteConfig = {
+  copyright: `Copyright © ${new Date().getFullYear()} Desmond Obisi. All Rights Reserved.`,
+  author: {
+    name: "Desmond Obisi",
+    accounts: [
+      {
+        url: "https://github.com/DesmondSanctity",
+        label: "Github Account",
+        type: "gray",
+        icon: <FaGithub />
+      },
+      {
+        url: "https://twitter.com/desmondcutest",
+        label: "Twitter Account",
+        type: "twitter",
+        icon: <FaTwitter />
+      },
+      {
+        url: "https://dev.to/m_ahmad",
+        label: "Dev Account",
+        type: "gray",
+        icon: <FaDev />
+      },
+      {
+        url: "https://linkedin.com/in/muhammad-ahmad20",
+        label: "LinkedIn Account",
+        type: "linkedin",
+        icon: <FaLinkedin />
+      },
+      {
+        url: "https://www.quora.com/profile/Muhammad-Ahmad-66",
+        label: "Quora Account",
+        type: "red",
+        icon: <FaQuora />
+      },
+      {
+        url: "mailto:muhammad.ahmad8043@gmail.com",
+        label: "Mail ahmad",
+        type: "gray",
+        icon: <FiMail />
+      }
+    ]
+  }
+};
+
+export default siteConfig;
