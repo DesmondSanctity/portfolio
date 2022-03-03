70dfee3d5d6a713b03927d1413e2d0d57f01b8ea
Desmond Obisi <desmondcutest@gmail.com>
2022-03-03 07:43:35 +0100

    remove blog

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,19 @@
 import * as React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import About from "./about";
-import Posts from "./blog/posts";
+// import Posts from "./blog/posts";
 import RepositoriesList from "./repositories-list";
 import Home from "./home";
 import TechStack from "./tech-stack";
-import NotebookPost from "./blog/notebook-app/notebook-post";
+// import NotebookPost from "./blog/notebook-app/notebook-post";
 
 const routes = [
   { path: "/", exact: true, name: "Home", component: Home },
   { path: "/about", exact: true, name: "About", component: About },
-  {
-    path: "/blog/notebook-app",
-    exact: true,
-    name: "Post",
-    component: NotebookPost
-  },
-  {
-    path: "/open-source",
-    exact: true,
-    name: "OpenSource",
-    component: RepositoriesList
-  },
-  { path: "/blog", exact: true, name: "Blog", component: Posts },
-  { path: "/tech-stack", exact: true, name: "Tools", component: TechStack }
+  // { path: "/blog/notebook-app", exact: true, name: "Post", component: NotebookPost },
+  { path: "/open-source", exact: true, name: "OpenSource", component: RepositoriesList },
+  // { path: "/blog", exact: true, name: "Blog", component: Posts },
+  { path: "/tech-stack", exact: true, name: "Tools", component: TechStack },
 ];
 const Navigation = () => {
   return (
@@ -33,7 +23,8 @@ const Navigation = () => {
           key={idx}
           exact={route.exact}
           path={route.path}
-          render={props => <route.component {...props} />}
+          render ={() => <route.component />}
+          // render={(props) => <route.component {...props} />}
         />
       ))}
       <Redirect to="/" />
